0fc73ad3edb51fdfe4fed35b91c444521b14ec2c
zifrit <zifrit@list.ru>
2022-08-10 18:20:03 +0300

    .

--- MainPartSiteDGU/Students/models.py
+++ MainPartSiteDGU/Students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+
+
+class InfoStudent(models.Model):
+    first_name = models.CharField(max_length=255, verbose_name='Имя')
+    last_name = models.CharField(max_length=255, verbose_name='Фамилия')
+    middle_name = models.CharField(max_length=255, verbose_name='Отчество')
+     
+    student_status = models.ForeignKey(to=SS, verbose_name='статус студента', on_delete=models.CASCADE(), default=1)
+    social_status = models.BooleanField(verbose_name='социальная справка')
+
--- MainPartSiteDGU/Students/views.py
+++ MainPartSiteDGU/Students/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import generic
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+#
+# class RegisterS(generic.CreateView):
+#     model = pass
