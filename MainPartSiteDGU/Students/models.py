425deadcdcebc4cc3cc4582d64a0551a32ee4027
zifrit <zifrit@list.ru>
2022-08-11 18:00:26 +0300

    add new model StatusStudent and TypeCourse, and edit model InfoStudent add new fields

--- MainPartSiteDGU/Students/models.py
+++ MainPartSiteDGU/Students/models.py
@@ -17,24 +17,55 @@ class InfoStudent(models.Model):
     middle_name = models.CharField(
         max_length=255, verbose_name='Отчество'
     )
+
+    direction = models.ForeignKey(
+        to='TypeCourse',
+        verbose_name='Направление',
+        on_delete=models.CASCADE,
+    )
+    TYPE_STATUS = (
+        (1, '1-курс'),
+        (2, '2-курс'),
+        (3, '3-курс'),
+        (4, '4-курс'),
+    )
+    course = models.IntegerField(
+        choices=TYPE_STATUS,
+        default=0
+    )
     photo_student = models.FileField(
         upload_to=directory_path,
         verbose_name='фото студента'
     )
     student_status = models.ForeignKey(
-        to='S_S',
+        to='StatusStudent',
         verbose_name='статус студента',
-        on_delete=models.CASCADE(),
+        on_delete=models.CASCADE,
         default=1
     )
     social_status = models.BooleanField(
         verbose_name='социальная справка'
     )
 
+    class Meta:
+        db_table = 'InfoStudent'
 
-class S_S(models.Model):
+
+class StatusStudent(models.Model):
     name = models.CharField(
         verbose_name='статус',
-        default='студент',
-        null=True
+        max_length=255,
     )
+
+    class Meta:
+        db_table = 'StatusStudent'
+
+
+class TypeCourse(models.Model):
+    name = models.CharField(
+        verbose_name='Направление',
+        max_length=255,
+    )
+
+    class Meta:
+        db_table = 'TypeCourse'
