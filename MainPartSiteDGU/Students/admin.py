fcead7529c5ef6a6865f3d66fc362c8afec50ab9
zifrit <zifrit@list.ru>
2022-08-11 18:48:01 +0300

    .

--- MainPartSiteDGU/Students/admin.py
+++ MainPartSiteDGU/Students/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(StatusStudent)
+admin.site.register(TypeDirection)
+
+@admin.register(InfoStudent)
+class AdminInfoStudent(admin.ModelAdmin):
+    list_display = [
+        'last_name',
+        'first_name',
+        'direction',
+        'course',
+
+    ]
+    list_filter = ['time_create',]
--- MainPartSiteDGU/Students/models.py
+++ MainPartSiteDGU/Students/models.py
@@ -4,68 +4,76 @@ from django.db import models
 # Create your models here.
 
 def directory_path(instance, filename):
-    return f'photo/%Y/%m.%d/'
+    return f'photo/%Y/%m.%d/{filename}'
 
 
 class InfoStudent(models.Model):
-    first_name = models.CharField(
-        max_length=255, verbose_name='Имя'
-    )
-    last_name = models.CharField(
-        max_length=255, verbose_name='Фамилия'
-    )
-    middle_name = models.CharField(
-        max_length=255, verbose_name='Отчество'
-    )
+    first_name = models.CharField(max_length=255,
+                                  verbose_name='Имя'
+                                  )
+    last_name = models.CharField(max_length=255,
+                                 verbose_name='Фамилия'
+                                 )
+    middle_name = models.CharField(max_length=255,
+                                   verbose_name='Отчество'
+                                   )
 
-    direction = models.ForeignKey(
-        to='TypeCourse',
-        verbose_name='Направление',
-        on_delete=models.CASCADE,
-    )
+    direction = models.ForeignKey(to='Students.TypeDirection',
+                                  verbose_name='Направление',
+                                  on_delete=models.CASCADE,
+                                  )
     TYPE_STATUS = (
         (1, '1-курс'),
         (2, '2-курс'),
         (3, '3-курс'),
         (4, '4-курс'),
     )
-    course = models.IntegerField(
-        choices=TYPE_STATUS,
-        default=0
-    )
-    photo_student = models.FileField(
-        upload_to=directory_path,
-        verbose_name='фото студента'
-    )
-    student_status = models.ForeignKey(
-        to='StatusStudent',
-        verbose_name='статус студента',
-        on_delete=models.CASCADE,
-        default=1
-    )
-    social_status = models.BooleanField(
-        verbose_name='социальная справка'
-    )
+    course = models.IntegerField(choices=TYPE_STATUS,
+                                 default=0
+                                 )
+    photo_student = models.FileField(upload_to=directory_path,
+                                     verbose_name='фото студента'
+                                     )
+    student_status = models.ForeignKey(to='Students.StatusStudent',
+                                       verbose_name='статус студента',
+                                       on_delete=models.CASCADE,
+                                       default=1
+                                       )
+    social_status = models.BooleanField(verbose_name='социальная справка',
+                                        )
+    time_create = models.DateTimeField(auto_now_add=True,
+                                       verbose_name='дата создания'
+                                       )
+    time_update = models.DateTimeField(auto_now=True,
+                                       verbose_name='дата изменения'
+                                       )
 
     class Meta:
         db_table = 'InfoStudent'
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
 
 class StatusStudent(models.Model):
-    name = models.CharField(
-        verbose_name='статус',
-        max_length=255,
-    )
+    name = models.CharField(verbose_name='статус',
+                            max_length=255,
+                            )
 
     class Meta:
         db_table = 'StatusStudent'
 
+    def __str__(self):
+        return self.name
 
-class TypeCourse(models.Model):
-    name = models.CharField(
-        verbose_name='Направление',
-        max_length=255,
-    )
+
+class TypeDirection(models.Model):
+    name = models.CharField(verbose_name='Направление',
+                            max_length=255,
+                            )
 
     class Meta:
-        db_table = 'TypeCourse'
+        db_table = 'TypeDirection'
+
+    def __str__(self):
+        return self.name
