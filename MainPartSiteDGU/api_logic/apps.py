080d16d6fad8f117efab936da39b2a0d0e11dbc7
zifrit <zifrit@list.ru>
2023-03-13 16:06:41 +0300

    создал 3 приложения для раздиления приложения апи и студента на раздели апи студента, апи логики студента и приложения студента, логики студента

---
+++ MainPartSiteDGU/api_logic/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiLogicConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_logic'
---
+++ MainPartSiteDGU/logic_for_student/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LogicForStudentConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'logic_for_student'
